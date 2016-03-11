734aa31fa09b675132290b949ef5e0bf6cd5af1d
Taylor Sargent <sarg0049@algonquinlive.com>
2016-03-11 08:52:30 -0500

    added more variables

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 var $header = $('header');
 var $h1 = $('h1');
 var $main = $('main');
+var $p = $('p');
+var $h2 = $('h2');
+var $img = $('img');
+var $dl = $('dl');
+var $dt = $('dt');
 
 $header.addClass('masthead');
-$h1.addClass('intro').remove('big-heading');
-
+$h1.addClass('logo').remove('big-heading');
 $main.addClass('wrapper');
+
+** $p.addClass('intro');
+
+
+$h2.addClass('slug-head');
+
+** main.forEach(function (li){
+  var $li = $
+})
+
+$img.addClass('slug-img');
+
+$dl.addClass('classification');
+$dt.remove('big-label');
